c43f0d0df1d2172b93aa6c60f7b89fa5aa87b82e
FMirabdollahii <mirabdollahifatemeh053@gmail.com>
2023-05-24 23:17:01 +0330

    completing code

--- app2.js
+++ app2.js
@@ -2,10 +2,19 @@ const app = Vue.createApp({
     
     data(){
         return{
+            x : 0,
+            y : 0,
+
             showBooks : true,
             subject :"bof koor",
             author : "Sadegh Hedayat",
-            age : 45
+            age : 45 ,
+            
+            // this is Array
+            books: [
+                {title : "Atomic Habits" , subject : "This book is about success"},
+                {title: "Man Advardo Nistam", subject : "This book is about life of good rich man"}
+            ]
             
         }
     },
@@ -16,7 +25,13 @@ const app = Vue.createApp({
 
         handleEvent () {
             console.log("event");
+        },
+        
+        handleMoseMove(e){
+            this.x= e.offsetX;
+            this.y= e.offsetY;
         }
+
     }
         
     }
